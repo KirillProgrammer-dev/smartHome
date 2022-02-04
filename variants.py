37763fbc328a86f503675794a91dd7b0ad2200f5
KirillProgrammer-dev <kirilkenig@gmail.com>
2022-02-04 13:21:29 +0300

    upd

--- variants.py
+++ variants.py
@@ -1,4 +1,9 @@
+# -*- encoding: utf-8 -*-
 import random
+import re
+import requests
+from aiohttp import request
+from bs4 import BeautifulSoup
 
 
 class Variants:
@@ -9,6 +14,14 @@ class Variants:
         self.howAreYouResponses = ['Где-то между хорошо и очень хорошо.', 'После того, как ты спросил, намного лучше.', 'Спасибо, что спросил, ты сделал мой день намного лучше.', 'Лучше, чем у многих людей.', 'Как у тебя, но лучше.', 'Ничего особенного.', 'Не жалуюсь, все равно меня никто не слушает.', 'Достаточно хорошо.', 'Средне, не великолепно, не ужасно, просто средне.', 'Пока все в порядке.']
         self.howAreYou = ['как дела']
         self.Hello = ['привет', 'хай', 'bonjour', 'hi', 'шалом', 'здравствуй', 'салют']
+        self.Time = ['время', 'часов']
+        self.Wiki = ["что такое"]
+        self.offense = ['дура', "дебил"]
+        self.answersOffence = ["Кто так обзывается - тот так и называется", "Я так, и обидится могу"]
+        self.getStories = ["расскажи сказку", "сказка"]
+        self.clothes = ["одеть", "надеть"]
+        self.veryCold = ["Сегодня очень холодно, наденьте куртку и теплую обувь", "Сегодня холодно, советую надеть утеплое нижнее бельё"]
+        self.warm = ["Сегодня прохладно, советую надеть толстовку и джинсы", ""]
 
     def getWeather(self):
         return self.weather
@@ -49,3 +62,76 @@ class Variants:
     
     def getHello(self):
         return self.Hello
+    
+    def getTime(self):
+        return self.Time
+    
+    def getWiki(self):
+        return self.Wiki
+    
+    def yandexAnswer(self, zadanie:str):
+        url = "https://www.google.ru/search?q=" + zadanie.strip().replace("зона", "")
+        response = requests.get(url)
+        response.encoding = "utf-8"
+        #soup = BeautifulSoup(response.text, 'lxml')
+        #quotes = soup.find_all('tr', class_='BNeawe')
+        #if quotes == []: quotes = soup.find_all('div', class_='BNeawe')
+        #print(soup.decode('cp1251').encode('utf8'))
+        #import json
+
+        #data = json.loads(response.text)
+        print(response.json())
+
+    def getOffense(self):
+        return self.offense
+    
+    def answerOffence(self):
+        return self.answersOffence[random.randint(0, len(self.answersOffence) - 1)]
+    
+    def getStory(self):
+        url = "https://nukadeti.ru/skazki/dlya_malchikov"
+        all_stories_list = []
+        all_stories = requests.get(url)
+        all_stories.encoding = "utf-8"
+        soup_stories = BeautifulSoup(all_stories.text, 'lxml')
+        stories = soup_stories.find_all('div', class_='info')
+        for story in stories:
+            all_stories_list.append(story.a.get('href'))
+        
+        story_response = requests.get("https://nukadeti.ru" + all_stories_list[random.randint(0, len(all_stories_list) - 1)])
+        story_response.encoding = "utf-8"
+        soup_story = BeautifulSoup(story_response.text, 'lxml')
+        text = soup_story.find_all('div', class_='tale-text')
+        string = ""
+        for sentence in text[0].p:
+            string += sentence
+        
+        return string
+
+    def answerStory(self):
+        return self.getStories
+    
+    def getClothes(self):
+        return self.clothes
+    
+    def answerClothes(self, weather):
+        if weather <= -10:
+            return ""
+
+        elif weather <= 0 and weather > -10:
+            return ""
+
+        elif weather > 0 and weather <= 10:
+            return ""
+
+        elif weather > 10 and weather <= 20:
+            return ""
+
+        elif weather > 20 and weather <= 30:
+            return ""
+
+        elif weather > 30 and weather <= 40:
+            return ""
+        
+        elif weather > 40:
+            return ""
--- voice.py
+++ voice.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-from pyttsx3 import engine
+from numpy import var
 import speech_recognition as sr
 import os
 import sys
@@ -11,10 +11,17 @@ from datetime import date
 from getMainPartOfWord import Stemmer as getWord
 from variants import Variants 
 from config import Config
+import wikipedia 
+import re
+import socket
+import bluetooth
+
+user_ip = socket.gethostbyname(socket.gethostname())
+print(user_ip)
+wikipedia.set_lang("ru") 
 
 def normalize_city_name(name):
     return name.strip().lower().replace('ё', 'е')
-
 
 cities = {normalize_city_name(x) for x in open("data/cities.txt", "r").readlines() if x.strip()}
 openweatherApiKey = '658a409e6023c63e869f6b066d96bca4'
@@ -26,14 +33,64 @@ get_word = getWord()
 variants = Variants()
 config = Config()
 
-def talk(words):
+def talk(words): #function to synthesize human-like speech
 	try:
 		print(words)
 		os.system("cd ~")
-		os.system(f'bash /home/hacker/speech.sh "{words}"')
+		os.system(f'bash speech.sh "{words}"')
 	except ValueError:
-		pass
+		print("Error")
 
+def on_light():
+	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+	nearby_devices = bluetooth.discover_devices(lookup_names=True)
+	for addr, name in nearby_devices:
+		if name == "Bluetooth_light":
+			break
+
+	sock.connect((addr, 1))
+	
+	sock.send("1")
+
+
+def getWeather(zadanie, user_ip):
+	city = None
+	for i in cities:
+		zadanie = variants.deletePrepositions(zadanie)
+		zadanie = variants.deleteVariants(zadanie, variants.getWeather())
+		zadanie = variants.deleteQuestions(zadanie)
+		zadanie = variants.prepareWord(zadanie)
+		if (get_word.stem(variants.prepareWord(i)) == get_word.stem(zadanie)):
+			city = i
+	if city == None:
+		url = f"https://ip-geo-location.p.rapidapi.com/ip/{user_ip}"
+
+		querystring = {"format":"json"}
+
+		headers = {
+			'x-rapidapi-host': "ip-geo-location.p.rapidapi.com",
+			'x-rapidapi-key': "cdbf458776msh6db1411cfa434cap1da535jsncb6d50067b80"
+			}
+
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		print(response)
+
+	if (city != None):
+		openweatherApi = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweatherApiKey}&units=metric'
+		weatherRequest = requests.get(openweatherApi)
+		weatherJson = json.loads(weatherRequest.text)
+		weather = weatherJson["main"]["temp"]
+		gradus = 'градусов'
+		if (abs(int(weather)) % 10 == 1):
+			gradus = 'градус'
+		elif (abs(int(weather)) % 10 in [2, 3, 4]):
+			gradus = 'градуса'
+		elif (abs(int(weather)) % 10 in [5, 6, 7, 8, 9, 0]):
+			gradus = 'градусов'
+		return (str(round(weather)) + " " + gradus, weather)
+	else:
+		return ('Я не поняла название города', None)
 
 def command():
 	r = sr.Recognizer()
@@ -49,53 +106,66 @@ def command():
 		zadanie = command()
 	return zadanie
 
-def makeSomething(zadanie):
+def makeSomething(zadanie): #main fuction to process sentensec and answer
+	answers = 0
 	if config.getName() in zadanie:
+		print("Yes")
 		zadanie = zadanie.replace(config.getName(), '')
 		if 'открыть сайт' in zadanie:
 			talk("Уже открываю")
 			url = 'https://itproger.com'
+			answers = 1
 			webbrowser.open(url)
+
 		elif 'стоп' in zadanie:
 			talk("Да, конечно, без проблем")
 			sys.exit()
+			answers = 1
+
 		elif 'имя' in zadanie:
 			talk('Меня зовут ' + (config.getName()).capitalize())
-		elif variants.getMatches(variants.getWeather(), zadanie):
-			city = None
-			for i in cities:
-				zadanie = variants.deletePrepositions(zadanie)
-				zadanie = variants.deleteVariants(zadanie, variants.getWeather())
-				zadanie = variants.deleteQuestions(zadanie)
-				zadanie = variants.prepareWord(zadanie)
-				if (get_word.stem(variants.prepareWord(i)) == get_word.stem(zadanie)):
-					city = i
 
-			if (city != None):
-				openweatherApi = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweatherApiKey}&units=metric'
-				weatherRequest = requests.get(openweatherApi)
-				weatherJson = json.loads(weatherRequest.text)
-				weather = weatherJson["main"]["temp"]
-				gradus = 'градусов'
-				if (abs(int(weather)) % 10 == 1):
-					gradus = 'градус'
-				elif (abs(int(weather)) % 10 in [2, 3, 4]):
-					gradus = 'градуса'
-				elif (abs(int(weather)) % 10 in [5, 6, 7, 8, 9, 0]):
-					gradus = 'градусов'
-				talk(str(round(weather)) + " " + gradus)
-			else:
-				talk('Я не поняла название города')
+		elif variants.getMatches(variants.getWeather(), zadanie):
+			talk(getWeather(zadanie, user_ip)[0])
+			answers = 1
+		
+		elif variants.getMatches(variants.getClothes(), zadanie):
+			talk(variants.answerClothes(getWeather(zadanie, user_ip)[1]))
+			answers = 1
+
 		elif 'число' in zadanie and 'какое' in zadanie:
 			date = datetime.date.today().day
 			talk(dictNumbers[date])
+			answers = 1
+
 		elif variants.getMatches(variants.getHowAreYou(), zadanie):
 			talk(variants.responseHowAreYou())
+			answers = 1
 		
-		elif variants.getMatches(variants.getHello(), zadanie):
-			talk(variants.responseHello() + '!')
+		elif variants.getMatches(variants.getOffense(), zadanie):
+			talk(variants.answerOffence())
+			answers = 1
 
+		elif variants.getMatches(variants.answerStory(), zadanie):
+			talk("Хорошо")
+			talk(variants.getStory())
+			answers = 1
 
+		elif not answers:
+			try:
+				if wikipedia.page(variants.deleteVariants(str(zadanie).replace("зона", ""), variants.getWiki())):
+					talk(re.sub(r"\(.+?\)\s", "", wikipedia.page(variants.deleteVariants(str(zadanie).replace("зона", ""), variants.getWiki())).content).replace("«", "").replace("»", "").split(".")[0])
+			except wikipedia.exceptions.DisambiguationError as e:
+				talk("К сожелению я не могу выполнить данное задание")
+			
+			except wikipedia.exceptions.PageError: 
+				talk("К сожелению я не могу выполнить данное задание")
+		#elif variants.getMatches(variants.getWiki(), zadanie):
+		#	talk(re.sub(r"\(.+?\)\s", "", wikipedia.page(variants.deleteVariants(zadanie, variants.getWiki())).content).replace("«", "").replace("»", "").split(".")[0])
+		
+
+		#elif variants.yandexAnswer(zadanie):
+		#	talk(variants.yandexAnswer(zadanie)[0])
 talk('Привет, это твой голосовой помощник ' + (config.getName()).capitalize())
 
 while True:
